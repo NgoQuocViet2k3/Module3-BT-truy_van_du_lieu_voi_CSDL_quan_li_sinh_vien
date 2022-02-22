a5ab78b953701cdc9497a75554ed7a73948d52a4
ruukichi <ngovietlc93@gmail.com>
2022-02-22 15:57:08 +0700

    add

--- truy_van_du_lieu_voi_CSDL_quan_tri_sinh_vien.sql
+++ truy_van_du_lieu_voi_CSDL_quan_tri_sinh_vien.sql
@@ -1,12 +1,15 @@
-use quanlisinhvien;
-select*from student;
-select*from student
-where Status = true;
-select*from subject
-where Credit <10;
-select S.StudentID,S.StudentName,C.ClassName
-from student S join Class C on S.ClassID = C.ClassID
-where C.ClassName = 'A1';
-select S.StudentID,S.StudentName,Sub.SubName,M.Mark
-from student S join Mark M on S.StudentID = M.StudentID join subject Sub on M.SubID = Sub.SubID
-WHERE Sub.SubName = 'CF';
+use quantrisinhvien;
+select *
+from student
+where StudentName like 'h%';
+select *
+from class
+where month(StartDate) = 12;
+select *
+from subject
+where Credit between 3 and 5;
+update student
+set ClassID = 2 where StudentName = "Hung";
+select S.StudentName,Sub.SubName,M.Mark 
+from Student S join Mark M on S.StudentID = M.StudentID join subject Sub on Sub.SubID = M.SubID
+order by M.Mark DESC,StudentName asc;
